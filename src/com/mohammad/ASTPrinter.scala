3a4bb6b65773269960773712737e402c84b6c2d5
Mohammad Siavashi <mohammad.siav@outlook.com>
2020-05-22 19:02:37 +0430

    Add nodes details

--- src/com/mohammad/ASTPrinter.scala
+++ src/com/mohammad/ASTPrinter.scala
@@ -28,7 +28,29 @@ class ASTPrinter(val global: Global) extends Plugin{
           for(t <- trees){
             ast = ast + showRaw(t)
           }
+          getASTNodes(ast)
+          println();
+          println("Tree representaion: \n")
           prettyPrint(ast)
+      }
+
+      def getASTNodes(input: String) {
+        var nodes: Array[String] = Array[String] ()
+        var tmpNode: String = ""
+        println("Nodes (without duplicate): ")
+        input.foreach {
+          case '(' | ')' | ',' => {
+            if( (tmpNode != "") && !nodes.contains(tmpNode) ){
+              println("  " + tmpNode)
+              nodes :+= tmpNode
+            }
+            tmpNode = "";
+          }
+          case ' ' =>
+          case f => tmpNode += f 
+        }
+        println()
+        println("Number of nodes (without duplicate): " + nodes.length)
       }
 
       def prettyPrint(input: String) = {
